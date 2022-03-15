69884ffef094a25e2a1f643dfe3e226d97b72069
Claudio Filho <claudiofilho025@gmail.com>
2022-03-14 23:44:27 -0300

    Primeiro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TodoList';
+  items:string [] = [];
+  tarefa: string = "";
+
+ emitirEvento(): void{
+   this.items.push(this.tarefa)
+ }
+ 
+
+ addTarefa(){
+   this.items.push(this.tarefa)
+ }
+ 
+ deletarTarefas(item:string){
+   this.items.splice(this.items.indexOf(item),1)
+ }
+
+ hoverIt:boolean;
+  constructor(){
+      this.hoverIt = false;
+  }
+
+  @Input()
+  toDoItem:string = ""
+
+  @Output() 
+  deletev: EventEmitter<any> = new EventEmitter<any>()
+
+  deletef(){
+    this.deletev.emit(this.toDoItem)
+  }
 }
+
+
+
+
+
